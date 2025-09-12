1cecd649b905ae7a1a035fd69051237da9d74038
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-12 23:27:37 +0530

    Initial Version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # memer.rs
 a meme generation rust cli tool
+
+## Features
+- generate a meme from any text (uses dalle 3 from openai), it will output the image by a argument that will be passed, simple yet intresting :)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use async_openai::{
+    Client,
+    types::{CreateImageRequestArgs, ImageResponseFormat, ImageSize},
+};
+use clap::Parser;
+use std::error::Error;
+use uuid::Uuid;
+
+fn get_default_uuid_file_name(file_type: Option<String>) -> String {
+    format!(
+        "{}.{}",
+        Uuid::new_v4(),
+        file_type.unwrap_or_else(|| "png".to_string()),
+    )
+}
+
+#[derive(Parser, Debug)]
+#[command(name="memer", version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    query: String,
+
+    #[arg(short, long, default_value_t=get_default_uuid_file_name(None))]
+    file: String,
+}
+
+async fn generate_image(query: &str, dir: &str) -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+    let request = CreateImageRequestArgs::default()
+        .prompt(query)
+        .response_format(ImageResponseFormat::Url)
+        .size(ImageSize::S1024x1024)
+        .user("memer")
+        .build()?;
+    let response = client.images().create(request).await?;
+    response.save(dir).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    generate_image(&args.query, &args.file).await.unwrap();
 }
