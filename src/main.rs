734eac6265468c6cd167cc92f69d1079e24016fc
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-27 23:37:00 +0530

    WIP Progress Bar

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ use clap::Parser;
 use clap_verbosity_flag::Verbosity;
 use dotenv::dotenv;
 use log::{debug, info};
+use memer_rs::utils::{get_progress_bar, increment_progress_bar};
+mod config;
 mod utils;
 
 #[derive(Parser, Debug)]
 #[command(name="memer", author, version, about, long_about=None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(long = "query")]
     query: String,
 
     #[arg(short, long, default_value_t=get_default_uuid_directory())]
@@ -23,9 +25,11 @@ struct Args {
     verbosity: Verbosity,
 }
 
-async fn generate_image(query: &str, dir: &str) -> Result<(), OpenAIError> {
+async fn generate_image(query: &str, dir: &str) -> Result<String, OpenAIError> {
     info!("generating image for query: {}", query);
+    increment_progress_bar().await;
     let client = Client::new();
+    increment_progress_bar().await;
     debug!("creating image generation request");
     let request = CreateImageRequestArgs::default()
         .prompt(load_prompt(query))
@@ -33,12 +37,20 @@ async fn generate_image(query: &str, dir: &str) -> Result<(), OpenAIError> {
         .size(ImageSize::S1024x1024)
         .model(ImageModel::DallE3)
         .build()?;
+    increment_progress_bar().await;
     debug!("sending request to OpenAI");
+    increment_progress_bar().await;
     let response = client.images().create(request).await?;
+    increment_progress_bar().await;
     info!("saving image to directory: {}", dir);
-    response.save(dir).await?;
+    let saved_paths = response.save(dir).await?;
+    increment_progress_bar().await;
     info!("image saved successfully");
-    Ok(())
+    let saved_path = saved_paths.get(0);
+    if saved_path.is_none() {
+        "".to_owned() // TODO: Fix with Custom Errors
+    }
+    Ok(saved_path.to_owned())
 }
 
 #[tokio::main]
@@ -49,4 +61,7 @@ async fn main() {
         .filter_level(cli.verbosity.log_level_filter())
         .init();
     generate_image(&cli.query, &cli.folder).await.unwrap();
+    get_progress_bar()
+        .await
+        .finish_with_message("Image Generated at");
 }
